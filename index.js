618db11f09810aa5593e8c69c61fb41114104a81
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-08-26 20:16:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'bitch I\'m a cow';
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 
